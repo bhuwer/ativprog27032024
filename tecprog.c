0cca4e09f7db4b84e3c7fad79a6bb8ad7376c5d9
bhuwer <165963774+bhuwer@users.noreply.github.com>
2024-04-03 19:30:19 -0300

    Add files via upload

---
+++ tecprog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+	
+	int main() {
+    int altura, linha, coluna;
+
+    printf("altura do triangulo:");
+    scanf("%d", &altura);
+
+    for (linha = 0; linha < altura; linha++) {
+        for (coluna = 0; coluna <= linha; coluna++) {
+            printf("#");
+        }
+        printf("\n");
+    }
+    return 0;
+}
